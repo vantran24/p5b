827549c40b8e1aaffcbb77f395cc8fd75b7bfe33
Van <vvtran@wisc.edu>
2013-12-13 19:27:49 -0600

    finished filestat

--- user/filestat.c
+++ user/filestat.c
@@ -23,7 +23,13 @@ main(int argc, char *argv[])
  //call stat() in user.h
  if (stat(pathname, st) == -1){
 	 printf(1, "something wrong in stat\n");
+	 exit();
  }
+ printf(1, "File: %s\n", pathname);
+ printf(1, "Size: %d\n", st->size);
+ printf(1, "Type: %d\n", st->type);
+ printf(1, "Checksum: %s\n", st->checksum);
+
  //print everything
 
   exit();
